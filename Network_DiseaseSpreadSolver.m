c8ce16b6a1c0766b46dfd3057da725a274da526b
lbothwell <lb14292@my.bristol.ac.uk>
2017-04-24 13:01:17 +0100

    Temperature formula added

--- Network_DiseaseSpreadSolver.m
+++ Network_DiseaseSpreadSolver.m
@@ -3,7 +3,7 @@
 % Network disease spread solver
 %
 %
-function [z] = Network_DiseaseSpreadSolver()
+function [] = Network_DiseaseSpreadSolver()
 TimeScale = [0 1];
 IC = [50; 4000; 60; 70; 55; 42; 73; 100; 200; 320; 1200; 450; 10; 5];
 n = length(IC)/2; % number of towns that are connected
@@ -11,6 +11,9 @@ global ConnectionMat
 ConnectionMat = [0 1 1 1 0 0 0; 1 0 1 0 1 0 0; 1 1 0 1 0 1 0; ...
                  1 0 1 0 0 1 0; 0 1 0 0 0 1 1; 0 0 1 1 1 0 1;...
                  0 0 0 0 1 1 0];
+             
+global Temp
+Temp = 25*ones(100,1); 
 for i = 1:n
     for j = i:n
         ConnectionMat(j,i) = ConnectionMat(i,j);
@@ -19,10 +22,13 @@ for i = 1:n
         end
     end
 end
+
+
+
 %ConnectionMat = zeros(n);
 Connectivity = mean(sum(ConnectionMat));
 
-[T, z] = ode45(@Network_DiseaseSpread, TimeScale, IC);
+[t, z] = ode45(@Network_DiseaseSpread, TimeScale, IC);
 figure()
-plot(T,z(:,1:n))
+plot(t,z(:,1:n))
 end
