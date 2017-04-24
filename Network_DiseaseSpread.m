9acb0e9349c788bc88055294894f1e8e5c0afab4
lbothwell <lb14292@my.bristol.ac.uk>
2017-04-24 13:02:46 +0100

    Temperature vector added

--- Network_DiseaseSpread.m
+++ Network_DiseaseSpread.m
@@ -7,7 +7,7 @@ global Temp
 N = 1000; % number of humans
 M = 100000; % number of Mosquitoes
 
-% b = 0.000203*Temp*(Temp - 11.7)*sqrt(42.3 - Temp)
+%b = 0.000203*Temp(t)*(Temp(t) - 11.7)*sqrt(42.3 - Temp(t));
 b = 0.1; %biting rate 
  
 g = 0.5; % rate of recovery rate
